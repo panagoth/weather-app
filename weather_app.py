2fa3303a39d12ee8feefea1ecbd74f65e8c10f66
panagoth <72699960+panagoth@users.noreply.github.com>
2025-05-16 07:09:18 +0300

    weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -2,7 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Load .env file
+load_dotenv()
 
 API_KEY = os.getenv("WEATHER_API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
